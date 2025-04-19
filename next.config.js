6a7173725d9ce877ac220b1666b273f96f41a459
Orizu <michael@limpiar.online>
2025-04-19 12:50:06 +0100

    new

--- next.config.js
+++ next.config.js
@@ -3,19 +3,17 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore lint errors during build
+  },
   webpack(config, { isServer }) {
-    // Optional: Modify Webpack configuration if needed for styles
     if (!isServer) {
       config.resolve.fallback = { fs: false };
     }
     return config;
   },
-  // Optional: Enable CSS minification
-  cssModules: true,
-  css: {
-    loaderOptions: {
-      // Add CSS options here if you need them
-    },
+  images: {
+    domains: ['limpiar-backend.onrender.com'],
   },
 };
 
